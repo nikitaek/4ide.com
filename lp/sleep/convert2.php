af40da4bd4d7679e36a0e44cc06f834874bafb1c
nikitaek <n0nam3@gmail.com>
2024-09-07 10:08:51 +0700

    1

--- lp/sleep/convert2.php
+++ lp/sleep/convert2.php
@@ -10,12 +10,10 @@ $_file = array(
 /** Download Google Spreadsheet. Make sure that it has public sharing accesss to read. Otherwise you will get error. */
 $file = "https://docs.google.com/spreadsheets/d/".$_file['key']."/gviz/tq?tqx=out:csv&sheet=".$_file['sheet'];
 
-
 $questions = [
   "config" => []
 ];
 $headerName = false;
-$lines = explode(PHP_EOL, file_get_contents($file));
 $current = array(
   "ID" => -1,
   "Type" => "",
@@ -34,123 +32,123 @@ $index = array(
   "extra" => 8
 );
 
-foreach ($lines as $line) {
-  $row = str_getcsv($line);
+if (($handle = fopen($file, "r")) !== FALSE) {
+  while (($row = fgetcsv($handle, 1000, ",")) !== FALSE) {
+    /** Some lines might be disabled on level of question and on level of answer. So lets skip them.
+     * Notice : a possible place for a bug, if question is disabled but answers are enabled - we will have problem, because answers will be attached to previous enabled question. Heh
+     */
+    if($row[$index['Enabled']] == "Yes") {
 
-  /** Some lines might be disabled on level of question and on level of answer. So lets skip them.
-   * Notice : a possible place for a bug, if question is disabled but answers are enabled - we will have problem, because answers will be attached to previous enabled question. Heh
-   */
-  if($row[$index['Enabled']] == "Yes") {
+      //Lets detect if we are working with Question row or Answer row.
+      if($row[$index['Type']] && $row[$index['ID']] != "" ) {
+        //Question
 
-    //Lets detect if we are working with Question row or Answer row.
-    if($row[$index['Type']] && $row[$index['ID']] != "" ) {
-      //Question
+        $current['ID']++;
+        $current['Type']    = $row[$index['Type']];
+        $current['AID']     = 0;
 
-      $current['ID']++;
-      $current['Type']    = $row[$index['Type']];
-      $current['AID']     = 0;
+        $questions[$current['ID']] = [
+          /** Question ID, used in future to navigate between different questions */
+            "id"            => $row[$index['ID']],
+          /** Question Text, may contact HTML */
+            "question_text" => $row[$index['Text']],
+          /** Description is the text which is displayed under the title */
+            "description"   => $row[$index['Description']],
+          /** Type of the question. Important variable to know, how to handle the content parsing */
+            "type"          => $row[$index['Type']],
+          /** Strange name, but this is part of the legacy. Data about the answers goes here */
+            "variants"      => array(),
+          /** Flag to show or not next button. Not sure if it works on front end. For now, it is not working on server configuration side */
+            "next_btn"      => true,
+          /** Flag to show or not previous button. Not sure if it works on front end. For now, it is not working on server configuration side */
+            "prev_btn"      => true,
+          /** This wariable consist a place, where the user should be forwarded when he clicks NEXT.
+           * Possible options are
+           * id of question in the system
+           * https:// link to external website which will be opened in new window.
+           * 
+           * If there is a configuration variable "forward" on asnwer level, this one will overwrite previous configurations
+           */
+            "forward"       => $row[$index['Forward']] ?? false,
+          /** Name of GTM (google tag manager) function to call, upon pressing "next" on this questions */
+            "GTM"           => $row[$index['GTM']] ?? false,
+          /** A JSON of custom variables, as an extra container for more flexible configurations */
+            "extra"     => json_decode($row[$index['extra']]) ?? false
+        ];
 
-      $questions[$current['ID']] = [
-        /** Question ID, used in future to navigate between different questions */
-          "id"            => $row[$index['ID']],
-        /** Question Text, may contact HTML */
-          "question_text" => $row[$index['Text']],
-        /** Description is the text which is displayed under the title */
-          "description"   => $row[$index['Description']],
-        /** Type of the question. Important variable to know, how to handle the content parsing */
-          "type"          => $row[$index['Type']],
-        /** Strange name, but this is part of the legacy. Data about the answers goes here */
-          "variants"      => array(),
-        /** Flag to show or not next button. Not sure if it works on front end. For now, it is not working on server configuration side */
-          "next_btn"      => true,
-        /** Flag to show or not previous button. Not sure if it works on front end. For now, it is not working on server configuration side */
-          "prev_btn"      => true,
-        /** This wariable consist a place, where the user should be forwarded when he clicks NEXT.
-         * Possible options are
-         * id of question in the system
-         * https:// link to external website which will be opened in new window.
-         * 
-         * If there is a configuration variable "forward" on asnwer level, this one will overwrite previous configurations
-         */
-          "forward"       => $row[$index['Forward']] ?? false,
-        /** Name of GTM (google tag manager) function to call, upon pressing "next" on this questions */
-          "GTM"           => $row[$index['GTM']] ?? false,
-        /** A JSON of custom variables, as an extra container for more flexible configurations */
-          "extra"     => json_decode($row[$index['extra']]) ?? false
-      ];
+        switch($current['Type']) {
+          /** A type of question which is just an HTML block, title and HTML content which can include anything we decide on server side */
+          case "html" :
+            $questions[$current['ID']]['variants']['content'] = $row[$index['Text']];
+          break;
+          /** Very complicated type of page, which i am thinking to stop using. The problem is that it need a full additional configuration page to work. Further you will see a script which is trying to get those variables from spreadsheet and build a more customized version of final screen */
+          case "final-screen" :
+            $finalName = $row[$index['Text']];
 
-      switch($current['Type']) {
-        /** A type of question which is just an HTML block, title and HTML content which can include anything we decide on server side */
-        case "html" :
-          $questions[$current['ID']]['variants']['content'] = $row[$index['Text']];
-        break;
-        /** Very complicated type of page, which i am thinking to stop using. The problem is that it need a full additional configuration page to work. Further you will see a script which is trying to get those variables from spreadsheet and build a more customized version of final screen */
-        case "final-screen" :
-          $finalName = $row[$index['Text']];
+            include("final2.php");
+            $questions[$current['ID']]['variants'] = json_encode($finalResult);
+          break;
+        }
 
-          include("final2.php");
-          $questions[$current['ID']]['variants'] = json_encode($finalResult);
-        break;
-      }
+      } elseif (in_array($row[$index['Type']], array("name","config")) && !$row[$index['ID']]) {
+        // More technical elements
+        switch($row[$index['Type']]) {
+          case "name" :
+              $headerName = $row[$index['Text']];
+          break;
 
-    } elseif (in_array($row[$index['Type']], array("name","config")) && !$row[$index['ID']]) {
-      // More technical elements
-      switch($row[$index['Type']]) {
-        case "name" :
-            $headerName = $row[$index['Text']];
-        break;
+          case "config" :
+            $newConfig = json_decode($row[$index['Text']], true);
+            $questions['config'] = array_merge($questions['config'], $newConfig);
 
-        case "config" :
-          $newConfig = json_decode($row[$index['Text']], true);
-          $questions['config'] = array_merge($questions['config'], );
+            /** if we have a app_id variable, then we have to imprint it into original HTML file. Therefore we are trying to open the file, upon building this JSON and write in the variable. */
+            if($newConfig['app_id']) {
+              if($originalHTML = file_get_contents($headerName."/index.html")) {
+                $originalHTML = str_replace("{{appId}}", $newConfig['app_id'], $originalHTML);
 
-          /** if we have a app_id variable, then we have to imprint it into original HTML file. Therefore we are trying to open the file, upon building this JSON and write in the variable. */
-          if($newConfig['app_id']) {
-            if($originalHTML = file_get_contents($headerName."/index.html")) {
-              $originalHTML = str_replace("{{appId}}", $newConfig['app_id'], $originalHTML);
-
-              file_put_contents($headerName."/index.html", $originalHTML);
+                file_put_contents($headerName."/index.html", $originalHTML);
+              }
             }
-          }
-        break;
+          break;
+        }
+      } else {
+        //Answer
+
+        switch ($current['Type']) {
+          case "multi" :
+          case "single" :
+          case "grade" :
+          case "package" :
+          case "review" :
+            $questions[$current['ID']]['variants'][] = array(
+                // Title of the answer. In most cases displayed as the main text in the button
+                "title"     => $row[$index['Text']],
+                // Id of the answer. Mostly used for analytical purpoise
+                "id"        => $current['AID']++,
+                /** Forwarding rule, if user selects this answer then he may be :
+                 * 1. if value is number then it will be redirected to another question
+                 * 2. if value starts with https:// then the window will be opened in new window
+                 */
+                "forward"   => $row[$index['Forward']],
+                // We were trying to implement a system which will calculate answers grades, and then on summary of those answers we will provide different final page. Not used any more, too complicated to infidels.
+                "grade"     => (float) $row[$index['Grade']],
+                "GTM"       => $row[$index['GTM']] ?? false,
+                "extra"     => json_decode($row[$index['extra']]) ?? false
+            );
+          break;
+
+          case "input" :
+
+          break;
+        }
       }
+
     } else {
-      //Answer
-
-      switch ($current['Type']) {
-        case "multi" :
-        case "single" :
-        case "grade" :
-        case "package" :
-        case "review" :
-          $questions[$current['ID']]['variants'][] = array(
-              // Title of the answer. In most cases displayed as the main text in the button
-              "title"     => $row[$index['Text']],
-              // Id of the answer. Mostly used for analytical purpoise
-              "id"        => $current['AID']++,
-              /** Forwarding rule, if user selects this answer then he may be :
-               * 1. if value is number then it will be redirected to another question
-               * 2. if value starts with https:// then the window will be opened in new window
-               */
-              "forward"   => $row[$index['Forward']],
-              // We were trying to implement a system which will calculate answers grades, and then on summary of those answers we will provide different final page. Not used any more, too complicated to infidels.
-              "grade"     => (float) $row[$index['Grade']],
-              "GTM"       => $row[$index['GTM']] ?? false,
-              "extra"     => json_decode($row[$index['extra']]) ?? false
-          );
-        break;
-
-        case "input" :
-
-        break;
-      }
+      // This answer or question are not enabled, so lets skip analyzing it
     }
-
-  } else {
-    // This answer or question are not enabled, so lets skip analyzing it
   }
+  fclose($handle);
 }
-
 
 if($headerName) {
   header("Code-Name : ". $headerName);
